4e9a742fe6edd69e0c9a26df90b596e90ff96937
♗CσNϙUҽRσR_MҽSƙEƚҽҽR♗ <74372091+CoNqUeRoRmEsKeTtEr@users.noreply.github.com>
2021-02-01 19:14:36 +0000

    Update __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -281,7 +281,7 @@ def get_help(bot: Bot, update: Update):
         update.effective_message.reply_text("Contact me in PM to get the list of possible commands.",
                                             reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="⚜️Help",url="t.me/{}?start=help".format(bot.username))],  
-                                                [InlineKeyboardButton(text="🛡Contact Creator",url="https://t.me/Surv_ivor")]]))
+                                                [InlineKeyboardButton(text="🛡Contact Creator",url="https://t.me/♗CσNϙUҽRσR_MҽSƙEƚҽҽR")]]))
         return
 
     elif len(args) >= 2 and any(args[1].lower() == x for x in HELPABLE):
