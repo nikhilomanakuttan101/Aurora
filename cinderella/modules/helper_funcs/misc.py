7971dea24a7e2b2c011955c48761845f1e6f60c9
♗CσNϙUҽRσR_MҽSƙEƚҽҽR♗ <74372091+CoNqUeRoRmEsKeTtEr@users.noreply.github.com>
2021-02-04 02:55:34 +0000

    Update misc.py

--- cinderella/modules/helper_funcs/misc.py
+++ cinderella/modules/helper_funcs/misc.py
@@ -68,12 +68,12 @@ def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     # can only have a certain amount of buttons side by side
     if len(pairs) > 7:
         pairs = pairs[modulo_page * 7:7 * (modulo_page + 1)] + [
-            (EqInlineKeyboardButton("<<<", callback_data="{}_prev({})".format(prefix, modulo_page)),
-                EqInlineKeyboardButton("🦋 Home", callback_data="bot_start"),
-             EqInlineKeyboardButton(">>>", callback_data="{}_next({})".format(prefix, modulo_page)))]
+            (EqInlineKeyboardButton("⫷ ⫷⫷", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("🦋⃟ Home", callback_data="bot_start"),
+             EqInlineKeyboardButton("『", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("🦋 Home", callback_data="bot_start")]]
+        pairs += [[EqInlineKeyboardButton("🦋⃟ Home", callback_data="bot_start")]]
 
 
     return pairs
